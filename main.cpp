9aa33a1886047ab90d267ac3fca5498abf261319
Michael Schuff <38889280+michaelschuff@users.noreply.github.com>
2020-09-30 09:28:18 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include "ResourcePath.hpp"
+
+using namespace sf;
+using namespace std;
+
+int main(int, char const**) {
+    
+    RenderWindow window(VideoMode(800, 600), "SFML window");
+
+
+    while (window.isOpen()) {
+        Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == Event::Closed) {
+                window.close();
+            }
+
+            if (event.type == Event::KeyPressed && event.key.code == Keyboard::Escape) {
+                window.close();
+            }
+        }
+
+        window.clear();
+
+        window.display();
+    }
+
+    return EXIT_SUCCESS;
+}
