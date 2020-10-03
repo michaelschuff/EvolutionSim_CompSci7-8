7ae01fecac4e66165a19117f7b22c4b3e56b1436
Bronte <brontemckinnis@gmail.com>
2020-10-03 16:19:07 -0700

    classes
    Started working on organizing the agent parent class.
    
    15 min

---
+++ class.cpp
@@ -0,0 +1,20 @@
+/*
+This is the whales classes and basic functions.
+*/
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <math.h>
+#include <vector>
+#include "vector3.hpp"
+#include "triangle.hpp"
+#include "mesh.hpp"
+#include "camera.hpp"
+using namespace sf;
+using namespace std;
+
+class Agent
+{
+public:
+    applyTexture(Texture&);
+    Sprite agentSprite;
+};
