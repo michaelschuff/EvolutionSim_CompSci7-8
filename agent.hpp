037f653f4c3bc7f8dee4be94a5fb86a842544234
Elaine <esdemetrion@gmail.com>
2020-10-06 13:35:45 -0700

    Created basic parent class as hpp file
    Also added fish.hpp

--- agent.hpp
+++ agent.hpp
@@ -1,6 +1,15 @@
-#ifndef AGENT_HPP_INCLUDED
+#ifndef agent_hpp
 #define AGENT_HPP_INCLUDED
 
+#include<iostream>
+#include "vector3.hpp"
 
+class agent {
+public:
+    vector3 position;
+    vector3 velocity;
+
+    int id;
+};
 
 #endif // AGENT_HPP_INCLUDED
---
+++ fish.h
@@ -0,0 +1,6 @@
+#ifndef FISH_H_INCLUDED
+#define FISH_H_INCLUDED
+
+
+
+#endif // FISH_H_INCLUDED
