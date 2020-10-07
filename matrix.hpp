9c6b991854acc3eade87c005c7901a3c3e897588
Michael <mschuff0881@gmail.com>
2020-10-07 10:07:30 -0700

    no message

---
+++ matrix.hpp
@@ -0,0 +1,13 @@
+//
+//  matrix.hpp
+//  CompSci78
+//
+//  Created by Michael Schuff on 10/4/20.
+//  Copyright © 2020 Michael Schuff. All rights reserved.
+//
+
+#ifndef matrix_h
+#define matrix_h
+
+
+#endif /* matrix_h */
---
+++ quaternion.hpp
@@ -0,0 +1,13 @@
+//
+//  quaternion.hpp
+//  CompSci78
+//
+//  Created by Michael Schuff on 10/4/20.
+//  Copyright © 2020 Michael Schuff. All rights reserved.
+//
+
+#ifndef quaternion_h
+#define quaternion_h
+
+
+#endif /* quaternion_h */
