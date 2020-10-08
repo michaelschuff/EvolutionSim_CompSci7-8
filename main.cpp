1b24a58eb108653976e02d97cc386248b970bbba
Bronte <brontemckinnis@gmail.com>
2020-10-08 13:35:36 -0700

    changes to whale.hpp and main.cpp
    Added some functions to whale.hpp to set up whale class and tested in the main

--- main.cpp
+++ main.cpp
@@ -2,98 +2,15 @@
 #include <iostream>
 #include <math.h>
 #include <vector>
-#include "vector3.hpp"
-#include "triangle.hpp"
-#include "mesh.hpp"
-#include "camera.hpp"
+#include "agent.hpp"
+#include "whale.hpp"
 //#include "ResourcePath.hpp"
 
 using namespace sf;
 using namespace std;
 
 
-RectangleShape Line(vector3, vector3);
-
-int main(int, char const**) {
-    const int width = 800, height = 800;
-    mesh cube({
-        // Front
-        triangle(vector3(0, 0, 0), vector3(0, 1, 0), vector3(1, 1, 0)),
-        triangle(vector3(0, 0, 0), vector3(1, 1, 0), vector3(1, 0, 0)),
-        
-        // Right
-        triangle(vector3(1, 0, 0), vector3(1, 1, 0), vector3(1, 1, 1)),
-        triangle(vector3(1, 0, 0), vector3(1, 1, 1), vector3(1, 0, 1)),
-        
-        // Back
-        triangle(vector3(1, 0, 1), vector3(1, 1, 1), vector3(0, 1, 1)),
-        triangle(vector3(1, 0, 1), vector3(0, 1, 1), vector3(0, 0, 1)),
-        
-        // Left
-        triangle(vector3(0, 0, 1), vector3(0, 1, 1), vector3(0, 1, 0)),
-        triangle(vector3(0, 0, 1), vector3(0, 1, 0), vector3(0, 0, 0)),
-        
-        // Top
-        triangle(vector3(0, 1, 0), vector3(0, 1, 1), vector3(1, 1, 1)),
-        triangle(vector3(0, 1, 0), vector3(1, 1, 1), vector3(1, 1, 0)),
-        
-        // Bottom
-        triangle(vector3(1, 0, 1), vector3(0, 0, 1), vector3(0, 0, 0)),
-        triangle(vector3(1, 0, 1), vector3(0, 0, 0), vector3(1, 0, 0))
-    });
-    
-    vector<vector3> verticies = {
-        vector3(-0.5, -0.5, -0.5),
-        vector3( 0.5, -0.5, -0.5),
-        vector3(-0.5,  0.5, -0.5),
-        vector3(-0.5, -0.5,  0.5),
-        vector3(-0.5,  0.5,  0.5),
-        vector3( 0.5, -0.5,  0.5),
-        vector3( 0.5,  0.5, -0.5),
-        vector3( 0.5,  0.5,  0.5),
-    };
-    
-    camera c(vector3(0, 0, 5), width, height, 0, 0, 0);
-    RenderWindow window(VideoMode(width, height), "SFML window");
-
-
-    window.setFramerateLimit(60);
-    
-    while (window.isOpen()) {
-        Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == Event::Closed) {
-                window.close();
-            }
-
-            if (event.type == Event::KeyPressed && event.key.code == Keyboard::Escape) {
-                window.close();
-            }
-        }
-
-        window.clear();
-        c.position.rotate(vector3(0, 1, 1), .5 * M_PI / 180.0);
-        
-        c.look_at(vector3(0, 0, 0));
-        vector<CircleShape> circles = c.get_screen(verticies);
-        for (int i = 0; i < circles.size(); i++) {
-            window.draw(circles[i]);
-        }
-        window.display();
-    }
-
-    return EXIT_SUCCESS;
+int main() {
+    whale newWhale(3, 2);
 }
 
-RectangleShape Line(vector3 v1, vector3 v2) {
-    v2.x = v2.x*50+400;
-    v2.z = v2.z*50+400;
-    v1.x = v1.x*50;
-    v1.z = v1.z*50;
-    RectangleShape line(Vector2f(sqrt(v1.x*v1.x+v1.z*v1.z),1));
-    line.setOrigin(0.5, 0.5);
-    line.rotate((atan2(v1.z,v1.x)*180.0/M_PI));
-    line.setPosition(v2.x, v2.z);
-    line.setFillColor(Color(150, 150, 150));
-    return line;
-}
--- whale.hpp
+++ whale.hpp
@@ -1,9 +1,39 @@
-#ifndef agent_hpp
-#define AGENT_HPP_INCLUDED
+#ifndef whale_hpp
+#define whale_hpp
 
 #include<iostream>
+#include<math.h>
+#include<time.h>
 #include "vector3.hpp"
+#include "agent.hpp"
 
+/*
+This file sets up the whale class, which is a child of the agent class.
+*/
 
+class whale : public agent
+{
+public:
+    whale(int, int);
+    int fishCounter; //how many fish the whale has eaten, used for ranking
+    sight (); //see the fish around them and determine which ones are in a distance of them
+    whaleMove (); //given a location, move towards it frame by frame
+    decision (); //using its traits, decide what to do on a turn
+    eatFish (); //reports which fish need to be removed from the list and adds to fishCounter
 
-#endif // WHALE_HPP_INCLUDED
+private:
+    int eatCloseFish; //trait that decides how much the whale is willing to move
+    int eatDenseFish; //trait that determines how efficient the whale is
+};
+
+whale::whale (int givenTraitClose, int givenTraitDense)
+{
+    int randChangeClose, randChangeDense;
+
+    fishCounter = 0;
+
+    //set up traits with some randomness
+    randChangeClose = (rand());
+}
+
+#endif /*whale_hpp*/
