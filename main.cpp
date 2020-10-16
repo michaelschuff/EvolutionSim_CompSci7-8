165ae897a3b2eacb51f78be2c3513158a9a9d02d
Bronte <brontemckinnis@gmail.com>
2020-10-16 14:22:41 -0700

    whale::sight function
    finished the whale sight function, which calculates the radius between the whale and the fish to see if they're close enough to eat. I used the pythagorean theorem to do this.

--- main.cpp
+++ main.cpp
@@ -29,37 +29,53 @@ int main() {
     time_t endTime;
     double timeDifference;
     vector<whale> whaleList;
+    vector<fish> fishList;
     int numWhales = 10; //the total number of whales that have ever existed
 
 
     //make whales w/ random starting traits
     for (int ww = 0; ww < numWhales; ww ++)
     {
-        whale newWhale((rand() % 10) + 1,(rand() % 10) + 1, vector3(), vector3(20,0,0));
+        whale newWhale((rand() % 10) + 1,(rand() % 10) + 1, vector3(), vector3(0,0,0));
+        newWhale.id = ww;
         whaleList.push_back(newWhale);
     }
 
+    //make fish
+    for (int ww = 0; ww < 10; ww ++)
+    {
+        fish newFish (vector3 ((rand() % 10), (rand() % 10), (rand() % 10)), vector3 ());
+        newFish.id = ww;
+        fishList.push_back(newFish);
+        cout << "x: " << fishList[ww].position.x << "y: " << fishList[ww].position.y << "z: " << fishList[ww].position.z << endl;
+    }
+
+    //see if fish can be eaten
+    whaleList[0].sight(fishList);
+
+    //see the ids of fish that can be eaten
+    for (int cc = 0; cc < whaleList[0].foodList.size(); cc++)
+    {
+        cout << "id: " << whaleList[0].foodList[cc] << endl;
+    }
+
+    /*
     //loop of program
-    //while(true)
-    //{
+    while(true)
+    {
         startTime = time(NULL);
 
-        cout << "size: " << whaleList.size() << endl;
-
-        for (int ll = 0; ll < whaleList.size(); ll++)
-        {
-            cout << whaleList[ll].position.x << endl;
-        }
 
         endTime = time(NULL);
 
         timeDifference = difftime(endTime, startTime);
 
-        //for (int ww = 0; ww < whaleList.size(); ww++)
-        //{
-          //  whaleList[ww].updatePosition (timeDifference);
-        //}
-    //}
+        for (int ww = 0; ww < whaleList.size(); ww++)
+        {
+            whaleList[ww].updatePosition (timeDifference);
+        }
+    }
+    */
 
     return EXIT_SUCCESS;
 }
--- whale.hpp
+++ whale.hpp
@@ -1,12 +1,21 @@
 #ifndef whale_hpp
 #define whale_hpp
+using namespace sf;
+using namespace std;
 
 #include<iostream>
 #include<math.h>
 #include<time.h>
+#include<vector>
+#include<stdio.h>
+#include<stdlib.h>
 #include "vector3.hpp"
 #include "agent.hpp"
 #include "fish.hpp"
+#include <cstdlib>
+#include <unistd.h>
+#include<ctime>
+
 
 /*
 This file sets up the whale class, which is a child of the agent class.
@@ -18,15 +27,16 @@ public:
     whale(int, int, vector3, vector3);
     int fishCounter; //how many fish the whale has eaten, used for ranking
     void updatePosition(double); //update position every frame based on velocity and time
-    //void sight (vector<fish>); //see the fish around them and determine which ones are in a distance of them
+    void sight (vector<fish>&); //see the fish around them and determine which ones are in a distance of them
     whaleMove (); //given a location, move towards it frame by frame
     decision (); //using its traits, decide what to do on a turn
-    eatFish (); //reports which fish need to be removed from the list and adds to fishCounter
+    vector<int> foodList; //reports which fish the whale might eat
 
 private:
     //both of these are on a scale of 1-10
     int eatCloseFish; //trait that decides how much the whale is willing to move
     int eatDenseFish; //trait that determines how efficient the whale is
+    int radius; //how far around the whale it can eat
 };
 
 whale::whale (int givenTraitClose, int givenTraitDense, vector3 pos, vector3 vel) : agent(pos, vel)
@@ -34,6 +44,7 @@ whale::whale (int givenTraitClose, int givenTraitDense, vector3 pos, vector3 vel
     int randChangeClose, randChangeDense, addOrSubtract;
 
     fishCounter = 0;
+    radius = 5;
 
 //set up traits with some randomness, based on a given initial value
     randChangeClose = (rand() % 5) - 2;
@@ -71,23 +82,28 @@ void whale::updatePosition (double timeDiff)
     position.z += velocity.z * timeDiff;
 }
 
-void whale::sight (vector<fish> possibleFood)
+void whale::sight (vector<fish> &fishList)
 {
     vector3 difference;
-    int eatRadius = 5;
-    int
+    double actualRadius;
 
-    for (int ff = 0; ff < possibleFood.size(); ff ++)
+    foodList.clear();
+
+    for (int ff = 0; ff < fishList.size(); ff ++)
     {
         //find the differences in x, y, and z values
-        difference.x = possibleFood[ff].position.x - position.x;
-        difference.y = possibleFood[ff].position.y - position.y;
-        difference.z = possibleFood[ff].position.z - position.z;
+        difference.x = fishList[ff].position.x - position.x;
+        difference.y = fishList[ff].position.y - position.y;
+        difference.z = fishList[ff].position.z - position.z;
 
         //use 3D Pythagorean theorem to find the radius value
-
+        actualRadius = sqrt(pow(difference.x, 2) + pow(difference.y, 2)+ pow(difference.z, 2));
 
         //if it's less than r add that fish's id to the list
+        if (actualRadius <= radius)
+        {
+            foodList.push_back(fishList[ff].id);
+        }
     }
 }
 
