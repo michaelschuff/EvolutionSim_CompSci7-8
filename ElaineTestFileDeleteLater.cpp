7f57e2286d8cb499037ee1572cfaee5cdc7bd3b1
Elaine <esdemetrion@gmail.com>
2020-10-17 23:24:40 -0700

    FishList initialization is different
    It is now a static variable in fish.hpp (since it should be the same for every fish). It is also assigned through pointers, since it is always the same as the vector of fish that will be kept in main.cpp. May want to put this in agent.hpp instead.

--- ElaineTestFileDeleteLater.cpp
+++ ElaineTestFileDeleteLater.cpp
@@ -12,22 +12,20 @@
 using namespace sf;
 using namespace std;
 
-//global variable. Turn into other type variable once I figure out how
+//global variables and static var instantiations. Turn into other type variable once I figure out how
 vector<fish> allFish = vector<fish>();
-
 int agent::nextID = 0; ///Necessary line
+vector<fish> *fish::allFishList = &allFish;
 
 int main(int, char const**) {
 
+
     //add new fish to the allAgents vector and update an id variable
     for(int ii = 0; ii < 10; ii ++) {
-        fish MrDarp(vector3(0, 100*(ii%5+1), ii*50), vector3(0, ii, 0));
-        allFish.push_back(MrDarp);
-        cout<<MrDarp.id<<endl;
-        fish JohnNash;
-        cout<<JohnNash.id<<endl;
+        allFish.push_back(fish(vector3(), vector3()));
+        allFish[ii].reportFishNumbers();
     }
-
+    allFish[0].reportFishNumbers();
 
     return EXIT_SUCCESS;
 }
--- fish.hpp
+++ fish.hpp
@@ -11,15 +11,19 @@ using namespace std;
 
 class fish : public agent{
 public:
-    //constructor--right now, it's just the same as the agent
+
+    static vector<fish> *allFishList; //pointer to vector in main. Every fish can see every other fish, so this is static
+
+    //constructor. Same as agent constructor for now.
     fish(vector3 pos = vector3(), vector3 vel = vector3()): agent(pos, vel)
     {
-
     }
+
     /*Function that should be run repeatedly in update area (while loop in main).
     Does everything that needs to be done repeatedly (updating velocity, position, physical appearance, etc)*/
     //void updateFish();
 
+    void reportFishNumbers();
 
 private:
     //function to calculate separation velocity. Returns vector3
@@ -34,9 +38,9 @@ private:
 };
 
 //allAgents is the list of all agent types. Returns a vector with only fish and not the agent itself. Yes, this works.
-std :: vector <fish> fish :: otherFishList(std::vector<fish> allAgents)
+std :: vector <fish> fish :: otherFishList(std::vector<fish> allFish)
 {
-    std::vector<fish> test = allAgents;
+    std::vector<fish> test = allFish;
 
     for(int ii = test.size()-1; ii >= 0; ii --)
     {
@@ -45,4 +49,15 @@ std :: vector <fish> fish :: otherFishList(std::vector<fish> allAgents)
     return test;
 }
 
+//For debugging purposes
+void fish :: reportFishNumbers()
+{
+    std::cout<<"This fish, (id #"<<id<<") knows there are "<<allFishList -> size()<<" fish in the sea"<<endl;
+    std::cout<<"    These fish are: ";
+    for(int ii = 0; ii < allFishList -> size(); ii ++) {
+        vector<fish> subFish = *allFishList;
+        cout<<subFish[ii].id<<", ";
+    }
+    std::cout<<endl;
+}
 #endif // FISH_HPP_INCLUDED
