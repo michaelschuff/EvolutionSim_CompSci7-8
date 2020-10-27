a9027da2fb021831b042d855200aaf3534c17bb4
Bronte <brontemckinnis@gmail.com>
2020-10-27 12:59:01 -0700

    Update vector3.hpp
    updated vector3 to match main

--- vector3.hpp
+++ vector3.hpp
@@ -12,38 +12,61 @@
 #include <iostream>
 #include <math.h>
 #include <assert.h>
+#include "color.hpp"
+#include "object.hpp"
 
-class vector3 {
+
+/**
+ * Rounds floating point number to a number of decimal places.
+ *
+ * @param n number
+ * @param decimals number of decimal places. 0 to cast to int, 1 to round to tenths, -1 to round to nearest 10
+ * @return rounded floating point number.
+ */
+float round(float n, int decimals) {
+    return round(pow(10, decimals) * n) / pow(10, decimals);
+}
+
+class quaternion;
+class vector3 : public object {
 public:
-    double x, y, z;
+    float x, y, z;
+    color c;
 
-    vector3() : x(0), y(0), z(0) {}
-    vector3(double x1, double y1, double z1) : x(x1), y(y1), z(z1) {}
-    vector3(const vector3 &v) : x(v.x), y(v.y), z(v.z) {}//copy constructor
-    
+    vector3() : object(), x(0), y(0), z(0) {}
+    vector3(float x1, float y1, float z1, color _c = color(), bool visable = true) : object(visable), x(x1), y(y1), z(z1), c(_c) {}
+    vector3(const vector3 &v, color _c, bool visable = true) : object(visable), x(v.x), y(v.y), z(v.z), c(_c) {}//copy constructor
+
     vector3 operator+(const vector3 &v);
     vector3 &operator+=(const vector3 &v);
     vector3 operator-(const vector3 &v);
+    vector3 operator-() const;
     vector3 &operator-=(const vector3 &v);
-    vector3 operator*(double k);
-    vector3 &operator*=(double k);
-    vector3 operator/(double k);
-    vector3 &operator/=(double k);
+    vector3 operator*(float k);
+    vector3 &operator*=(float k);
+    vector3 operator/(float k);
+    vector3 &operator/=(float k);
     vector3 &operator=(const vector3 &v);
-    double &operator[](int i);
-    
+    vector3 operator-();
+    float &operator[](int i) const;
+
     void cross(const vector3 &v);                       //vector becomes the cross product of the vector and the parameter
     void project_onto(vector3 v);
-    
-    void rotate(vector3 axis, double theta);            //rotates vector, clockwise by right-hand-rule
-    vector3 rotated(vector3 axis, double theta);        //returns rotated vector
-    
+
+    void rotate(vector3 axis, float theta);
+    void rotate(const quaternion &q);
+    vector3 rotated(vector3 axis, float theta);        //returns rotated vector
+    vector3 rotated(const quaternion &q);
+
+    float phi();
+    float theta();
     void normalize();                                   //sets magnitude to 1
     vector3 normalized();                               //returns vector with magnitude 1
-    double magnitude() const;
-    double distance(const vector3 &v);                  //distance between two vectors
-    std::string to_string() const;
-    void print() const;
+    float magnitude() const;
+    float square() const;
+    float distance(const vector3 &v);                  //distance between two vectors
+    std::string to_string();
+    void print();
 };
 
 vector3 vector3::operator+(const vector3 &v) {
@@ -61,6 +84,10 @@ vector3 vector3::operator-(const vector3 &v) {
     return vector3(x-v.x, y-v.y, z-v.z);
 }
 
+vector3 vector3::operator-() {
+    return vector3(-x, -y, -z);
+}
+
 vector3 &vector3::operator-=(const vector3 &v) {
     x -= v.x;
     y -= v.y;
@@ -68,27 +95,30 @@ vector3 &vector3::operator-=(const vector3 &v) {
     return *this;
 }
 
-vector3 vector3::operator*(double k) {      //for vector * double
+vector3 vector3::operator*(float k) {                  //for vector * float
     return vector3(k*x, k*y, k*z);
 }
 
-vector3 operator*(double k, const vector3 v) {    //for double * vector
+vector3 operator*(float k, const vector3 v) {          //for float * vector
     return vector3(k*v.x, k*v.y, k*v.z);
 }
 
-vector3 &vector3::operator*=(double k) {
+vector3 &vector3::operator*=(float k) {
     x *= k;
     y *= k;
     z *= k;
     return *this;
 }
 
-vector3 vector3::operator/(double k){
+vector3 vector3::operator/(float k){
     assert(k!=0);
     return vector3(x/k, y/k, z/k);
 }
 
-vector3 &vector3::operator/=(double k) {
+vector3 &vector3::operator/=(float k) {
+    if (k == 0) {
+        return;
+    }
     assert(k!=0);
     x /= k;
     y /= k;
@@ -103,20 +133,41 @@ vector3 &vector3::operator=(const vector3 &v) {
     return *this;
 }
 
-double &vector3::operator[](int i) {
-    assert(i>=0 && i<3);
-    switch (i) {
-        case 0:
-            return x;
-        case 1:
-            return y;
-        case 2:
-            return z;
-    }
+vector3 vector3::operator-() const {
+    return vector3(-x, -y, -z);
 }
 
-double vector3::magnitude() const {
+float &vector3::operator[](int i) const {
+    assert(i>=0 && i<3);
+    static float a;
+    switch (i) {
+        case 0:
+            a = x;
+            break;
+        case 1:
+            a = x;
+            break;
+        case 2:
+            a = x;
+            break;
+    }
+    return a;
+}
+
+float vector3::phi() {
+    return atan2(sqrt(x*x + z*z), y);
+}
+
+float vector3::theta() {
+    return atan2(round(z, 8), round(x, 8));
+}
+
+float vector3::magnitude() const {
     return sqrt(x*x + y*y + z*z);
+}
+
+float vector3::square() const {
+    return x*x + y*y + z*z;
 }
 
 void vector3::normalize() {
@@ -125,33 +176,32 @@ void vector3::normalize() {
 }
 
 vector3 vector3::normalized() {
-    double m = magnitude();
-    assert(m!=0);
+    float m = magnitude();
+    if (m == 0) {
+        return vector3();
+    }
     return vector3(x/m, y/m, z/m);
 }
 
-double vector3::distance(const vector3 &v) {
+float vector3::distance(const vector3 &v) {
     return (*this-v).magnitude();
 }
 
-double dot_product(const vector3 &v1, const vector3 &v2) {
+float dot_product(const vector3 &v1, const vector3 &v2) {
     return v1.x*v2.x + v1.y*v2.y + v1.z*v2.z;
 }
 
 void vector3::cross(const vector3 &v) {
-    double i = y*v.z - z*v.y;
-    double j = z*v.x - x*v.z;
-    double k = x*v.y - y*v.x;
+    float i = y*v.z - z*v.y;
+    float j = z*v.x - x*v.z;
+    float k = x*v.y - y*v.x;
     x = i;
     y = j;
     z = k;
 }
 
 vector3 cross_product(const vector3 &v1, const vector3 &v2) {
-    double i = v1.y*v2.z - v1.z*v2.y;
-    double j = v1.z*v2.x - v1.x*v2.z;
-    double k = v1.x*v2.y - v1.y*v2.x;
-    return vector3(i, j, k);
+    return vector3(v1.y*v2.z - v1.z*v2.y, v1.z*v2.x - v1.x*v2.z, v1.x*v2.y - v1.y*v2.x);
 }
 
 vector3 projection(vector3 v1, vector3 v2) { //project v1 onto v2
@@ -165,26 +215,19 @@ void vector3::project_onto(vector3 v) { //project v1 onto v2
     z = temp.z;
 }
 
-vector3 vector3::rotated(vector3 axis, double theta) {
-    axis.normalize();
-    double _sin = sin(theta);
-    double _cos = cos(theta);
-    return *this*_cos + _sin*cross_product(axis, *this) + axis*dot_product(*this, axis)*(1-_cos); //dont question the stack overflow gods
+std::string remove_terminating_zeros(std::string str) {
+    str.erase(str.find_last_not_of('0') + 1, std::string::npos);
+    str.erase(str.find_last_not_of('.') + 1, std::string::npos);
+    return str;
 }
 
-void vector3::rotate(vector3 axis, double theta) {
-    vector3 v = rotated(axis, theta);
-    x = v.x;
-    y = v.y;
-    z = v.z;
+std::string vector3::to_string() {
+    return ("<" + remove_terminating_zeros(std::to_string(x)) + ", " + remove_terminating_zeros(std::to_string(y)) + ", " + remove_terminating_zeros(std::to_string(z)) + ">");
 }
 
-
-std::string vector3::to_string() const {
-    return ("<" + std::to_string((int) (10*x)) + ", " + std::to_string((int) (10*y)) + ", " + std::to_string((int) (10*z)) + ">");
-}
-
-void vector3::print() const {
+void vector3::print() {
     std::cout << (*this).to_string() << std::endl;
 }
+
+
 #endif /* vector3_hpp */
