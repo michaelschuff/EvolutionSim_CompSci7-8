d7e8fa411f985ae95d9ecf0181d91201f9a04301
Michael <mschuff0881@gmail.com>
2020-10-30 13:22:40 -0700

    added everyones files

---
+++ fish.hpp
@@ -0,0 +1,198 @@
+//
+//  fish.hpp
+//  CompSci78
+//
+//  Created by Michael Schuff on 10/30/20.
+//  Copyright © 2020 Michael Schuff. All rights reserved.
+//
+
+#ifndef fish_hpp
+#define fish_hpp
+
+#include <iostream>
+#include <stdlib.h>
+#include <math.h>
+#include <vector>
+#include<SFML/Graphics.hpp>
+
+#include "vector3.hpp"
+#include "agent.hpp"
+using namespace std;
+
+class fish : public agent{
+public:
+    //added a comment here
+    //and again
+
+    static vector<fish>* allFishList; //pointer to vector in main. Every fish can see every other fish, so this is static
+
+    sf::CircleShape Shape2D; ///Delete later
+    double vision; ///could be private
+
+    //constructor. Same as agent constructor for now.
+    fish(vector3 pos = vector3(), vector3 vel = vector3()): agent(pos, vel)
+    {
+        srand(time(NULL));
+        vision = 50;
+        Shape2D.setRadius(10); ///Delete later
+    }
+
+    /*Function that should be run repeatedly in update area (while loop in main).
+    Does everything that needs to be done repeatedly (updating velocity, position, physical appearance, etc)*/
+    void updateFish();
+
+    void reportFishNumbers();
+
+private:
+    const double fishBoundary = 400;
+    void fixOffScreen();
+    ///these should only be used if there is more than one fish around
+    //function to calculate separation velocity. Returns vector3
+    vector3 vSeparation();
+    //function to calculate alignment velocity. Returns vector3
+    vector3 vAlignment();
+    //function to calculate cohesion velocity. Returns vector3
+    vector3 vCohesion();
+
+    //void to update velocity
+    void updateVelocity();
+    //void to update position based on current velocity
+    void updatePosition();
+
+    //function to make vector of only other fish
+    //std::vector <fish> otherFishList();
+
+};
+
+void fish :: updateFish()
+{
+    updateVelocity();
+    updatePosition();
+    fixOffScreen();
+    Shape2D.setPosition(position.x, position.y);
+}
+
+void fish :: fixOffScreen()
+{
+    vector3 screenCenter = vector3(500, 500, 0);
+    //if fish is off screen, move it to the opposite side and let it continue
+    if(abs(position.x - screenCenter.x) >= fishBoundary || abs(position.y - screenCenter.y) >= fishBoundary || abs(position.z - screenCenter.z) >= fishBoundary) {
+        velocity *= -1;
+    }
+}
+
+void fish :: updateVelocity()
+{
+    velocity += (3*vSeparation() + 0.2*vAlignment() + 2*vCohesion())*0.1;
+    if(velocity.magnitude() > 0) {
+        velocity.normalize(); //might wanna change this
+        velocity *= 1.5;
+    }
+}
+
+void fish :: updatePosition()
+{
+    position += velocity*0.5; //scale by something?
+}
+//should work
+vector3 fish :: vSeparation()
+{
+    vector3 finalV = vector3(0, 0, 0); //will become the returned vector3
+
+    float r = 20; ///How close fish can be to trigger this. Tweak this later!
+
+    //Evaluate each fish in the array
+    for(int ii = 0; ii < (*allFishList).size(); ii ++) {
+
+        if((*allFishList)[ii].id == id) { continue; }
+
+        double distAway = position.distance((*allFishList)[ii].position);
+
+        //if dist btwn me and this fish is less than r...
+        if(distAway < r && distAway > 0) {
+            //adjust finalV by: vector3 of movement away from fish, but move less for farther away fish
+            finalV += (position - (*allFishList)[ii].position).normalized()/distAway; //Possibly multiply by some value
+        }
+    }
+
+    ///may want to scale finalV by some amount
+    //finalV -= velocity;
+    if(finalV.magnitude() == 0) {return vector3(0, 0, 0);}
+    return finalV.normalized(); ///may want to scale differently
+}
+//should work
+vector3 fish :: vAlignment()
+{
+    vector3 finalV = vector3(0, 0, 0);
+    double fishConsidering;
+
+    //sum all other fish velocities (if fish can be seen)
+    for(int ii = 0; ii < (*allFishList).size(); ii ++) {
+        if((*allFishList)[ii].id == id) {continue;}
+        if(position.distance((*allFishList)[ii].position) < vision) {
+            fishConsidering ++;
+            finalV += (*allFishList)[ii].velocity;
+        }
+    }
+
+    //divide to get average
+    if(fishConsidering > 0 && finalV.magnitude() > 0) {
+        finalV /= fishConsidering;
+        return finalV.normalized();
+    }
+    return finalV;
+}
+
+//should work
+vector3 fish :: vCohesion()
+{
+    vector3 avgPos = vector3(0, 0, 0);
+    double fishConsidering = 0;
+
+    //add up all of the positions
+    for(int ii = 0; ii < (*allFishList).size(); ii ++) {
+        if((*allFishList)[ii].id == id) {continue;}
+        if(position.distance((*allFishList)[ii].position) < vision) {
+            fishConsidering ++;
+            avgPos += (*allFishList)[ii].position;
+        }
+    }
+    //make sure we don't divide by 0
+    if(fishConsidering > 0) {
+        avgPos /= fishConsidering;
+        //find vector from current position to average position
+        avgPos -= position;
+    }
+    //make sure we don't divide by 0
+    if(avgPos.magnitude() > 0) {
+        return avgPos.normalized();
+    }
+
+    return avgPos;
+}
+
+//allAgents is the list of all agent types. Returns a vector with only fish and not the agent itself. Yes, this works.
+/*std :: vector <fish> fish :: otherFishList()
+{
+    std::vector<fish> test = *allFishList;
+
+    for(int ii = test.size()-1; ii >= 0; ii --)
+    {
+        if(test[ii].id == id) { test.erase(test.begin() + ii); }
+    }
+    return test;
+}*/
+
+//For debugging purposes
+//void fish::reportFishNumbers()
+//{
+//    std::cout<<"This fish, (id #"<<id<<") knows there are "<<allFishList -> size()<<" fish in the sea"<<endl;
+//    std::cout<<"    These fish are: ";
+//    for(int ii = 0; ii < allFishList -> size(); ii ++) {
+//        vector<fish> subFish = *allFishList;
+//        cout<<subFish[ii].id<<", ";
+//    }
+//    std::cout<<endl;
+//}
+
+#endif /* fish_hpp */
--- whale.hpp
+++ whale.hpp
@@ -9,5 +9,210 @@
 #ifndef whale_hpp
 #define whale_hpp
 
+using namespace sf;
+using namespace std;
+
+#include<iostream>
+#include<math.h>
+#include<time.h>
+#include<vector>
+#include "vector3.hpp"
+#include "agent.hpp"
+#include "fish.hpp"
+#include <cstdlib>
+#include<cmath>
+
+
+/*
+This file sets up the whale class, which is a child of the agent class.
+*/
+
+class whale : public agent
+{
+public:
+    whale(int, int, vector3, vector3, vector3);
+    int fishCounter; //how many fish the whale has eaten, used for ranking
+    void updatePosition(double); //update position every frame based on velocity and time
+    whaleMove (); //given a location, move towards it frame by frame
+    void decision (vector<fish>&); //using its traits, decide what to do on a turn
+    vector<int> foodList; //reports which fish the whale might eat
+    bool eat; //whether or not to eat the fish
+    //both of these are on a scale of 1-10
+    int eatCloseFish; //trait that decides how much the whale is willing to move
+    int eatDenseFish; //trait that determines how efficient the whale is
+
+private:
+
+    int radius; //how far around the whale it can eat
+    long int volume; //the volume of water the fish can eat from
+    void decisionEat (int);
+    void decisionMove();
+    vector3 edges; //the bottom corner of the board
+    void sight (vector<fish>&); //see the fish around them and determine which ones are in a distance of them
+};
+
+whale::whale (int givenTraitClose, int givenTraitDense, vector3 pos, vector3 vel, vector3 boundary) : agent(pos, vel)
+{
+    int randChangeClose, randChangeDense, addOrSubtract;
+
+    fishCounter = 0;
+    radius = 50; //200 cm
+    volume = (4.0f/3.0f) * M_PI * (float)pow(radius, 3);
+    edges = boundary;
+
+//set up traits with some randomness, based on a given initial value
+    randChangeClose = (rand() % 5) - 2;
+    randChangeDense = (rand() % 5) - 2;
+
+    eatCloseFish = givenTraitClose + randChangeClose;
+    eatDenseFish = givenTraitDense + randChangeDense;
+
+    if (eatCloseFish > 10)
+    {
+        eatCloseFish = 10;
+    }
+    else if (eatCloseFish < 1)
+    {
+        eatCloseFish = 1;
+    }
+    if (eatDenseFish > 10)
+    {
+        eatDenseFish = 10;
+    }
+    else if (eatDenseFish < 1)
+    {
+        eatDenseFish = 1;
+    }
+
+//set velocity and position
+    velocity = vel;
+    position = pos;
+}
+
+void whale::updatePosition (double timeDiff)
+{
+    position.x += velocity.x * timeDiff;
+    position.y += velocity.y * timeDiff;
+    position.z += velocity.z * timeDiff;
+
+    //check if beyond boundaries
+    if (position.x > edges.x or position.x < 0)
+    {
+        position.x = abs(position.x - edges.x);
+    }
+
+    if (position.y > edges.y or position.y < 0)
+    {
+        position.y = abs(position.y - edges.y);
+    }
+
+    if (position.z > edges.z or position.z < 0)
+    {
+        position.z = abs(position.z - edges.z);
+    }
+}
+
+void whale::sight (vector<fish> &fishList)
+{
+    vector3 difference;
+    double actualRadius;
+
+    foodList.clear();
+
+    for (int ff = 0; ff < fishList.size(); ff ++)
+    {
+        //find the differences in x, y, and z values
+        difference.x = fishList[ff].position.x - position.x;
+        difference.y = fishList[ff].position.y - position.y;
+        difference.z = fishList[ff].position.z - position.z;
+
+        //use 3D Pythagorean theorem to find the radius value
+        actualRadius = sqrt(pow(difference.x, 2) + pow(difference.y, 2)+ pow(difference.z, 2));
+
+        //if it's less than r add that fish's id to the list
+        if (actualRadius <= radius)
+        {
+            foodList.push_back(fishList[ff].id);
+        }
+    }
+}
+
+void whale::decision (vector<fish> &fishList)
+{
+    sight(fishList);
+    decisionEat(fishList.size());
+
+    //eat fish
+    if (eat == true)
+    {
+        //don't move, in main go through foodList and remove those IDs
+        fishCounter += foodList.size();
+    }
+
+    //find destination to move
+    else
+    {
+        decisionMove();
+    }
+}
+
+void whale::decisionEat(int numFish)
+{
+    //numFish is the size of the fishList in main
+    float density;
+    float percentTotal;
+    eat = false;
+
+    //see if foodList is dense enoughs
+    density = ((float)foodList.size() / (float)volume) * 1000000.0f;
+
+    if (density >= eatDenseFish)
+    {
+        eat = true;
+    }
+
+    //otherwise, see if the whale is fine with eating far fish
+    else
+    {
+        percentTotal = ((float)foodList.size() / (float)numFish) * 100.0f;
+
+        if (percentTotal >= eatCloseFish)
+        {
+            eat = true;
+        }
+    }
+}
+
+void whale::decisionMove()
+{
+    vector3 tempVel;
+    float randX, randY, randZ;
+
+    //random movement if not eating
+    while (true)
+    {
+        //generate random values for velocity
+        randX = (rand() % 5) -2;
+        randY = (rand() % 5) -2;
+        randZ = (rand() % 5) -2;
+
+        if (randX != 0 & randY != 0 & randZ != 0)
+        {
+            break;
+        }
+    }
+
+    tempVel.x = randX;
+    tempVel.y = randY;
+    tempVel.z = randZ;
+
+    //normalize velocity
+    tempVel.normalize();
+
+    //scale it to 2
+    velocity.x = tempVel.x * 2;
+    velocity.y = tempVel.y * 2;
+    velocity.z = tempVel.z * 2;
+}
 
 #endif /* whale_hpp */
