73dafe6636d5b3d3b8ca1f84aa3b0197b8473e79
Elaine <esdemetrion@gmail.com>
2020-11-09 09:51:37 -0800

    hpp file added

---
+++ evolutionSim.hpp
@@ -0,0 +1,13 @@
+#ifndef EVOLUTIONSIM_HPP_INCLUDED
+#define EVOLUTIONSIM_HPP_INCLUDED
+#include <iostream>
+#include <stdlib.h>
+#include <math.h>
+#include <vector>
+#include "vector3.hpp"
+#include "agent.hpp"
+#include "fish.hpp"
+#include "whale.hpp"
+
+
+#endif // EVOLUTIONSIM_HPP_INCLUDED
