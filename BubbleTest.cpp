e937ff9bc67a0b9ec32bf2190d56330d7d41d6da
Elaine <esdemetrion@gmail.com>
2020-12-07 10:28:26 -0800

    Bubbles exist, test visualization

--- BubbleTest.cpp
+++ BubbleTest.cpp
@@ -17,8 +17,24 @@ using namespace std;
 
 int main()
 {
-    bubble FirstBubble(vector3(), vector3(), vector3());
-    return 0;
+    RenderWindow window(VideoMode(1020, 600), "Bubble Visualization");
+
+    bubble FirstBubble(vector3(500, 0, 500), vector3(-55, -55, 55), vector3(-55, -55, 55));
 
     //Make visualization of bubbles
+    while(window.isOpen())
+    {
+        Event event;
+        while(window.pollEvent(event))
+        {
+            if(event.type == Event::Closed)
+                window.close();
+        }
+        FirstBubble.updateBubble();
+        window.clear();
+        window.draw(FirstBubble.myShape);
+
+        window.display();
+    }
+    return 0;
 }
--- bubbles.hpp
+++ bubbles.hpp
@@ -1,5 +1,6 @@
 #ifndef BUBBLES_HPP_INCLUDED
 #define BUBBLES_HPP_INCLUDED
+#include<SFML/Graphics.hpp>
 #include<iostream>
 #include<stdlib.h>
 #include<math.h>
@@ -8,21 +9,44 @@
 
 class bubble {
 public:
+    sf::CircleShape myShape; ///debugging only
+
     bubble(vector3 pos, vector3 whaleVel, vector3 emissionVel) {
         setV0(whaleVel, emissionVel);
         position = pos;
+
+        ///debugging only, remove later
+        myShape.setRadius(20);
+        myShape.setFillColor(sf::Color(200, 200, 255));
     }
 
     void updateBubble();
+
 private:
     vector3 position, velocity;
+    float speed = 0.1;
     void setV0(vector3, vector3); //calculate the bubble's first velocity
+    void setNewVelocity();
 };
 
 void bubble::setV0(vector3 whaleVel, vector3 emissionVel) {
-    velocity = whaleVel + emissionVel; //actually I think this is physically accurate
+    velocity = (whaleVel + emissionVel).normalized(); //actually I think this is physically accurate
 
 }
 
+void bubble::updateBubble()
+{
+    ///deal with bubbles if they are out of bounds
+    setNewVelocity();
+    position += velocity;
+
+    myShape.setPosition(position.x, position.z); ///debugging only
+}
+
+void bubble::setNewVelocity()
+{
+    //could have something more fancy, but this is fine for now
+    velocity = speed * (velocity + vector3(0, 0, -0.0001)).normalized();
+}
 
 #endif // BUBBLES_HPP_INCLUDED
